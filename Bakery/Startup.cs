8d8d31ef7a1a42088a188bac3259d07cf306df99
fon12345 <yeofeja@gmail.com>
2019-12-21 15:35:30 +0100

    dont bakery 1.0

--- Bakery/Startup.cs
+++ Bakery/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bakery.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,24 +17,34 @@ namespace Bakery
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            // lägga till alla services vi behöver för applikationen samt dependencies
+
+            services.AddScoped<ICake, MockCake>(); // Dependency
+            services.AddControllersWithViews(); // MVC
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // håller reda på pipeline, vad som händer när sidan laddas
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage();   // environmentcheck för att se vilken mode kör på
             }
 
+            app.UseHttpsRedirection();  // ber programmet att köra https
+            app.UseStaticFiles(); // gör så att programmet kan köra statisk filer, bilder, javafiles etc
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Cake}/{Action=List}/{id?}"); // ändra default sida
             });
         }
     }
